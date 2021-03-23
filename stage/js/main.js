dbdf180b7be45fb5e3ad34521f6f2458d084a692
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2021-03-23 17:47:12 +0200

    create sidebare & page Header component

--- stage/js/main.js
+++ stage/js/main.js
@@ -2,6 +2,12 @@ $(function(){
     'use strict';
     $('.toggle-sidebar').on("click" , function (){
         $('.content-area, .sidebar').toggleClass("no-sidebar");
-
+    });
+    // Toggle submenu
+    $('.toggle-submenu').on("click" , function () {
+        $(this).find('.fa-angle-right').toggleClass('down');
+        $(this)
+            .next(".child-links")
+            .slideToggle();
     });
 });
